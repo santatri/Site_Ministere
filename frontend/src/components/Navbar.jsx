e8dc33e196dff01324656a3ed2ee3acc6b6461fc
toky <tokyherizo004@gmail.com>
2024-12-20 15:32:00 +0300

    Modification du navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import { FaSearch, FaPhone } from 'react-icons/fa';
+import { FaBars, FaClosedCaptioning, FaPhone, FaPlus, FaTimes, FaXRay, FaXbox } from 'react-icons/fa';
 import logo from '../assets/dgfop.png'; // Importez votre logo ici
 import '../styles/Navbar.css';
+import { BsFillMenuAppFill, BsMenuButton } from 'react-icons/bs';
 
 const Navbar = () => {
+  const [show, setShow] = useState(false)
+  const toggle = () => {
+    setShow(!show)
+    console.log(show);
+  }
+
   return (
     <nav className="navigation-bar">
       <div className="navbar-container">
@@ -14,17 +21,31 @@ const Navbar = () => {
         </div>
 
         {/* Navigation au centre */}
-        <div className="navbar-center">
-          <NavLink to="/home" className="nav-link" activeClassName="active-link">Accueil</NavLink>
-          <NavLink to="/bibliotheque" className="nav-link" activeClassName="active-link">Bibliothèque numérique</NavLink>
-          <NavLink to="/service" className="nav-link" activeClassName="active-link">Standard de Service</NavLink>
-          <NavLink to="/archives" className="nav-link" activeClassName="active-link">Actualités</NavLink>
-          <NavLink to="/about" className="nav-link" activeClassName="active-link">À propos</NavLink>
-          <NavLink to="/contact" className="nav-link" activeClassName="active-link">Contact</NavLink>
-        </div>
+        <div id={`${show?'':'navbar-center'}`} className={`${show?"show":"navbar-center"}`}>
+          <NavLink to="/home" className="nav-link" activeClassName="active-link">
+            Accueil
+          </NavLink>
+          <NavLink to="/bibliotheque" className="nav-link" activeClassName="active-link">
+            Bibliothèque numérique
+          </NavLink>
+          <NavLink to="/service" className="nav-link" activeClassName="active-link">
+            Standard de Service
+          </NavLink>
+          <NavLink to="/archives" className="nav-link" activeClassName="active-link">
+            Actualités
+          </NavLink>
+          <NavLink to="/about" className="nav-link" activeClassName="active-link">
+            À propos
+          </NavLink>
+          <NavLink to="/contact" className="nav-link" activeClassName="active-link">
+            Contact
+          </NavLink>
+          
+        </div> 
 
-        {/* Contact et recherche à droite */}
-        <div className="navbar-right">
+
+        {/* Contact rapide à droite */}
+        <div id='navbar-right' className="navbar-right">
           <div className="contact-quick">
             <FaPhone className="phone-icon" />
             <div className="contact-text">
@@ -32,8 +53,11 @@ const Navbar = () => {
               <span className="phone-number">+034 55 997 17</span>
             </div>
           </div>
-          
         </div>
+      <div className="burger">
+        {show?<FaTimes size={30} color={'red'}cursor={'pointer'}  onClick={toggle}/>: <FaBars size={30} color={'blue'}cursor={'pointer'} onClick={toggle}/>    }
+        
+      </div>
       </div>
     </nav>
   );
